a1c018236ff33a8a94b45dd1de4a5a82e69ebfaa
ecostabal <ecostabal@urbex.cl>
2023-12-04 23:13:01 -0300

    nuevoscambios

--- function.js
+++ function.js
@@ -1,10 +1,11 @@
 const axios = require('axios');
 const CryptoJS = require('crypto-js');
+const qs = require('qs'); // Asegúrate de tener instalado este paquete
 
 // Configuración de Flow
 const flowConfig = {
-    apiKey: '26FF859D-5E21-4E57-9BBD-7D0BAD8L06CC', // Reemplaza con tu API Key de Flow
-    secretKey: '3756483e4e45238c83a7fb6112a4aba948f83728', // Reemplaza con tu Secret Key de Flow
+    apiKey: '26FF859D-5E21-4E57-9BBD-7D0BAD8L06CC', // Reemplaza con tu API Key de Flow (almacenado en variables de entorno)
+    secretKey: '3756483e4e45238c83a7fb6112a4aba948f83728', // Reemplaza con tu Secret Key de Flow (almacenado en variables de entorno)
     // ...otros parámetros de configuración...
 };
 
@@ -15,10 +16,7 @@ const firmarParametros = (parametros, secretKey) => {
         .map(key => key + parametros[key])
         .join('');
 
-    // Utiliza el algoritmo SHA-256 y tu secretKey para firmar
-    const signature = CryptoJS.HmacSHA256(stringToSign, secretKey).toString(CryptoJS.enc.Hex);
-
-    return signature;
+    return CryptoJS.HmacSHA256(stringToSign, secretKey).toString(CryptoJS.enc.Hex);
 };
 
 // Función para manejar webhooks de Monday.com y generar link de pago con Flow
@@ -47,7 +45,7 @@ exports.generarLinkPagoFlow = async (req, res) => {
             query: query
         }, {
             headers: {
-                'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzMjg3MzUyNCwiYWFpIjoxMSwidWlkIjoyMzUzNzM2NCwiaWFkIjoiMjAyMy0wMS0zMVQyMTowMjoxNy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTUwNzUxNiwicmduIjoidXNlMSJ9.lX1RYu90B2JcH0QxITaF8ymd4d6dBes0FJHPI1mzSRE',
+                'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzMjg3MzUyNCwiYWFpIjoxMSwidWlkIjoyMzUzNzM2NCwiaWFkIjoiMjAyMy0wMS0zMVQyMTowMjoxNy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTUwNzUxNiwicmduIjoidXNlMSJ9.lX1RYu90B2JcH0QxITaF8ymd4d6dBes0FJHPI1mzSRE', // Reemplaza con tu API Key de Monday (almacenado en variables de entorno)
                 'Content-Type': 'application/json'
             }
         });
@@ -58,6 +56,7 @@ exports.generarLinkPagoFlow = async (req, res) => {
         const montoColumn = columnsData.find(column => column.id === 'n_meros03');
         const descripcionColumn = columnsData.find(column => column.id === 'ubicaci_n');
         const emailColumn = columnsData.find(column => column.id === 'correo_electr_nico');
+        const ordenTrabajoColumn = columnsData.find(column => column.id === 'id__de_elemento1')
 
         if (!montoColumn || !descripcionColumn || !emailColumn) {
             throw new Error('Datos necesarios no están presentes en el evento');
@@ -70,27 +69,58 @@ exports.generarLinkPagoFlow = async (req, res) => {
 
         const descripcion = descripcionColumn.text;
         const email = emailColumn.text;
+        const ordenNumber = ordenTrabajoColumn.text
 
         // Creación de la orden de pago para Flow
         const ordenCobro = {
             apiKey: flowConfig.apiKey,
+            commerceOrder: ordenNumber, // Asegúrate de reemplazar esto con un valor adecuado
             subject: descripcion,
             currency: 'CLP',
             amount: monto,
             email: email,
-            paymentMethod: 9,
-
+            urlConfirmation: 'https://tuservidor.com/confirmacion-pago', // URL de confirmación
+            urlReturn: 'https://tuservidor.com/confirmacion-pago', // URL de retorno
+            // Agrega aquí cualquier otro parámetro necesario
         };
+
+        // Función para crear una orden de pago
+        async function crearOrdenDePago() {
+            try {
+                // Realiza una solicitud POST a la API de Flow para crear la orden de pago
+                const flowResponse = await axios.post('https://www.flow.cl/api/payment/create', qs.stringify(ordenCobro), {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                });
+
+                // Verifica si se obtuvo una respuesta válida de Flow
+                if (!flowResponse.data || !flowResponse.data.token) {
+                    throw new Error('No se pudo obtener el token de pago de Flow');
+                }
+
+                // Procesa la respuesta de Flow (por ejemplo, redirecciona al usuario a la página de pago)
+                const token = flowResponse.data.token;
+                console.log('Token de pago de Flow:', token);
+
+                // Aquí puedes realizar acciones adicionales según tus necesidades, como redirigir al usuario a la página de pago de Flow.
+
+            } catch (error) {
+                console.error('Error al hacer la solicitud a Flow:', error);
+                // Maneja el error de manera adecuada (por ejemplo, envía una respuesta de error al cliente).
+            }
+        }
+
+        // Llama a la función para crear la orden de pago
+        crearOrdenDePago();
 
         // Firma de los parámetros con tu secretKey de Flow
         ordenCobro.s = firmarParametros(ordenCobro, flowConfig.secretKey);
 
-
         // Envío de la orden de pago a Flow
-        let flowResponse = await axios.post('https://www.flow.cl/api/payment/create', ordenCobro, {
+        let flowResponse = await axios.post('https://www.flow.cl/api/payment/create', qs.stringify(ordenCobro), {
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                // Aquí debes incluir cualquier otra cabecera necesaria, como autenticación
+                'Content-Type': 'application/x-www-form-urlencoded'
             }
         });
 
@@ -98,9 +128,8 @@ exports.generarLinkPagoFlow = async (req, res) => {
             throw new Error('No se pudo obtener el token de pago de Flow');
         }
 
-        const token = flowResponse.data.token;
-
         // Construir la URL de redirección
+        const token = flowResponse.data.token
         const urlRedireccion = `sandbox.flow.cl/app/web/pay.php?token=${token}`; // Reemplaza con tu URL de redirección
 
         // Actualizar el enlace en Monday.com
@@ -112,7 +141,7 @@ exports.generarLinkPagoFlow = async (req, res) => {
             }`
         }, {
             headers: {
-                'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzMjg3MzUyNCwiYWFpIjoxMSwidWlkIjoyMzUzNzM2NCwiaWFkIjoiMjAyMy0wMS0zMVQyMTowMjoxNy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTUwNzUxNiwicmduIjoidXNlMSJ9.lX1RYu90B2JcH0QxITaF8ymd4d6dBes0FJHPI1mzSRE',
+                'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzMjg3MzUyNCwiYWFpIjoxMSwidWlkIjoyMzUzNzM2NCwiaWFkIjoiMjAyMy0wMS0zMVQyMTowMjoxNy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTUwNzUxNiwicmduIjoidXNlMSJ9.lX1RYu90B2JcH0QxITaF8ymd4d6dBes0FJHPI1mzSRE', // Reemplaza con tu API Key de Monday (almacenado en variables de entorno)
                 'Content-Type': 'application/json'
             }
         });
